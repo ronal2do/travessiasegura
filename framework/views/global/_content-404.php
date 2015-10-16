f54b1965ecf8c118d9445fad163036e2b07978bb
Sotaque <sotaque@SOTMAC07.local>
2015-10-16 10:28:28 -0300

    first commit

---
+++ framework/views/global/_content-404.php
@@ -0,0 +1,12 @@
+<?php
+
+// =============================================================================
+// VIEWS/GLOBAL/_CONTENT-404.PHP
+// -----------------------------------------------------------------------------
+// Output for the 404 page.
+// =============================================================================
+
+?>
+
+<p class="center-text"><?php _e( 'A página que você está procurando não está mais aqui , ou nunca existiu em primeiro lugar. Você pode tentar procurar o que você está procurando usando o formulário abaixo . Se isso ainda não fornecer os resultados que você está procurando , você sempre pode começar de novo a partir da home page.', '__x__' ); ?></p>
+<?php get_search_form(); ?>
---
+++ framework/views/global/_content-none.php
@@ -0,0 +1,20 @@
+<?php
+
+// =============================================================================
+// VIEWS/GLOBAL/_CONTENT-NONE.PHP
+// -----------------------------------------------------------------------------
+// Output when no posts or pages are available.
+// =============================================================================
+
+?>
+
+<article id="post-0" class="post-0 post type-post status-publish hentry">
+  <div class="entry-wrap">
+    <header class="entry-header">
+      <h1 class="entry-title"><?php _e( 'Nada para exibir sobre isto', '__x__' ); ?></h1>
+    </header>
+    <div class="entry-content">
+      <p><?php _e( "Parece que você estava procurando não está mais aqui , ou talvez não estivesse aqui , para começar. Você pode querer tentar começar de novo a partir da homepage para ver se você pode encontrar o que você está depois de lá.", '__x__' ); ?></p>
+    </div>
+  </div>
+</article> <!-- end #post-0 -->
